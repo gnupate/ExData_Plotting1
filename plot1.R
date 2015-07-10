90f1dad6e0f1d8cb014e071093597f2b899b663d
gnupate <pat.eyler@gmail.com>
2015-07-10 14:17:11 -0600

    Plot1
    added R program and output for Plot 1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+library(dplyr) ## use the dplyr library
+
+## read in the data then filter out just the Feb 1 and 2 data from 2007
+airqual <- read.csv("household_power_consumption.txt", sep=";")
+aq <- filter(airqual, Date == "1/2/2007" | Date == "2/2/2007")
+
+## use the png filter to write to a graphics file device
+png(file="plot1.png")
+hist(as.numeric(as.character(aq$Global_active_power)), 
+     col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)"
+)
+dev.off()
