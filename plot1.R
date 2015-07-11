90516199cf2db99f54386e81bc1912133ee49f39
gnupate <pat.eyler@gmail.com>
2015-07-11 08:09:18 -0600

    homework complete
    All homework plots completed.
    
    Added a comment to each script indicating that the data is not in the
    current GitHub directory, but should be placed there to run the script

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
 library(dplyr) ## use the dplyr library
+## NOTE:  I have not saved the data to github, it should be unzipped into the directrory where this script is run
 
 ## read in the data then filter out just the Feb 1 and 2 data from 2007
 airqual <- read.csv("household_power_consumption.txt", sep=";")
