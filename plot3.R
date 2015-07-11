528b1ce471ce87f6536d48bc6f54429732615150
gnupate <pat.eyler@gmail.com>
2015-07-10 22:58:58 -0600

    Plot3
    Added code and output for plot 3

---
+++ plot3.R
@@ -0,0 +1,25 @@
+library(dplyr) ## use the dplyr library
+
+## read in the data then filter out just the Feb 1 and 2 data from 2007
+airqual <- read.csv("household_power_consumption.txt", sep=";")
+aq <- filter(airqual, Date == "1/2/2007" | Date == "2/2/2007")
+aq <- mutate(aq, datetime = paste(as.character(Date), as.character(Time), sep=" "))
+
+## use the png filter to write to a graphics file device
+png(file="plot3.png")
+
+plot(strptime(aq$datetime, "%d/%m/%Y %H:%M:%S"), as.numeric(as.character(aq$Sub_metering_1)), 
+         type="n", xlab="", ylab="Energy sub metering")
+
+points(strptime(aq$datetime, "%d/%m/%Y %H:%M:%S"), as.numeric(as.character(aq$Sub_metering_1)), 
+       type="l")
+
+points(strptime(aq$datetime, "%d/%m/%Y %H:%M:%S"), as.numeric(as.character(aq$Sub_metering_2)), 
+         type="l", col="red")
+
+points(strptime(aq$datetime, "%d/%m/%Y %H:%M:%S"), as.numeric(as.character(aq$Sub_metering_3)), 
+       type="l", col="blue")
+
+legend("topright", lwd=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.off()
